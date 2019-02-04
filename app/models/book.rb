a30b2ae3fb38f8a40542d86a531c22ab7e0d60d7
Paul Marclay <pmarclay@w3itsolutions.net>
2019-02-04 16:45:16 -0300

    Added validators for mandatory fields.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :gender, presence: true
+  validates :author, presence: true
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :year, presence: true
+  validates :publisher, presence: true
 end
