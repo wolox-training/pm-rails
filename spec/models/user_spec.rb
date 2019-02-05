48bd445edbf77c3ce7f13f99952d8fd6d75018b1
Paul Marclay <pmarclay@w3itsolutions.net>
2019-02-05 17:15:23 -0300

    installed rspec, factory_bot_rails and shoulda-matchers gems. I've also created factories and small tests for both User and Book models.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  subject(:user) { build(:user) }
+
+  it { is_expected.to be_valid }
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+
+end
