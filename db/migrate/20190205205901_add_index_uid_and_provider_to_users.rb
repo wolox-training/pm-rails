7a98929d9373b3ffda827891c615ade2ea2c335b
Paul Marclay <pmarclay@w3itsolutions.net>
2019-02-05 19:10:43 -0300

    implementing the new format of rails migrations, using 'change' instead of 'up/down' format

--- db/migrate/20190205205901_add_index_uid_and_provider_to_users.rb
+++ db/migrate/20190205205901_add_index_uid_and_provider_to_users.rb
@@ -1,9 +1,5 @@
 class AddIndexUidAndProviderToUsers < ActiveRecord::Migration[5.2]
-  def up
+  def change
     add_index :users, [:uid, :provider], unique: true
-  end
-
-  def down
-    remove_index :users, name: :index_users_on_uid_and_provider
   end
 end
