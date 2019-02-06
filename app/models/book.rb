b66dbe3fb0066742745459196fe4c304397d0f91
Paul Marclay <pmarclay@w3itsolutions.net>
2019-02-06 15:24:47 -0300

    - Added relationship between Book and Rent models. - Removed spec/models/rent_spec.rb, since we're not going to use it. - Added field validations in Rent model. - Better fields values in the rent factory.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
-  validates :gender, presence: true
+  validates :genre, presence: true
   validates :author, presence: true
   validates :image, presence: true
   validates :title, presence: true
   validates :year, presence: true
   validates :publisher, presence: true
+  has_many :rents
 end
--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,4 +1,9 @@
 class Rent < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :user_id, presence: true
+  validates :book_id, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
