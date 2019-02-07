882b40cb0e4c3f7c5cba69e89d4ad6d36c697107
Paul Marclay <pmarclay@w3itsolutions.net>
2019-02-07 17:45:57 -0300

    Changes
    - Added Devise::Test::ControllerHelpers to the spec/rails_helper file.
    - It has been added the BooksController class, to test index and show methods.
    - I've also added the `config.action_mailer.default_url_options` config to the test environment configuration file.
    - Added shared context file for authenticated user.

--- spec/controllers/api/v1/books_controller.rb
+++ spec/controllers/api/v1/books_controller.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
-# require 'shared_context'
+require 'support/shared_contexts/authenticated_user_context.rb'
 
 describe Api::V1::BooksController, type: :controller do
   include_context 'Authenticated User'
 
-  # describe 'GET #index' do
-  #   context 'When fetching all the users rents' do
-  #     let!(:rents) { create_list(:rent, 3, user: user) }
-  #
-  #     before do
-  #       get :index, params: { id: user.id }
-  #     end
-  #
-  #     it 'responses with the users rents json' do
-  #       expected = ActiveModel::Serializer::CollectionSerializer.new(
-  #           rents, each_serializer: RentSerializer
-  #       ).to_json
-  #       expect(response_body.to_json) =~ JSON.parse(expected)
-  #     end
-  #
-  #     it 'responds with 200 status' do
-  #       expect(response).to have_http_status(:ok)
-  #     end
-  #   end
-  # end
+  describe 'GET #index' do
+    context 'When fetching all the books' do
+      let!(:books) { create_list(:book, 3) }
+
+      before do
+        get :index
+      end
+
+      it 'responses with the books json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+            books, each_serializer: BookSerializer
+        ).to_json
+        expect(response.body) =~ JSON.parse(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 
   describe 'GET #show' do
     context 'When fetching a book' do
@@ -34,8 +34,8 @@ describe Api::V1::BooksController, type: :controller do
       end
 
       it 'responses with the book json' do
-        expect(response_body.to_json).to eq BookSerializer.new(
-            rent, root: false
+        expect(response.body).to eq BookSerializer.new(
+            book, root: false
         ).to_json
       end
 
