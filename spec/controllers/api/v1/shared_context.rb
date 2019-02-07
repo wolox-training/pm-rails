a5de23cbe36b6e9e8c8c530b5c06210152986340
Paul Marclay <pmarclay@w3itsolutions.net>
2019-02-07 17:45:57 -0300

    Changes
    - Added Devise::Test::ControllerHelpers to the spec/rails_helper file.
    - It has been added the BooksController class, to test index and show methods.
    - I've also added the `config.action_mailer.default_url_options` config to the test environment configuration file.
    - Added shared context file for authenticated user.

--- spec/controllers/api/v1/shared_context.rb
+++
@@ -1,7 +0,0 @@
-RSpec.shared_context 'Authenticated User', :shared_context => :metadata do
-  let(:user) { create(:user) }
-
-  before do
-    request.headers.merge! user.create_new_auth_token
-  end
-end
