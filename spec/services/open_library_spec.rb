ff4913df61f7f72c510716ff4649aa842ef1a840
Paul Marclay <pmarclay@w3itsolutions.net>
2019-02-21 19:34:43 -0300

    - Changed the Net::HTTP.get for OpenLibrary.new.find_by_isbn.

--- spec/services/open_library_spec.rb
+++ spec/services/open_library_spec.rb
@@ -5,31 +5,29 @@ describe OpenLibrary do
     stub_request(:get, 'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579&format=json&jscmd=data')
         .to_return(
             status: 200,
-            body: load_fixure('get_response_success'),
+            body: load_fixture('get_response_success'),
             headers: { 'Content-Type': 'application/json' }
         )
 
     stub_request(:get, 'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579abc&format=json&jscmd=data')
         .to_return(
             status: 200,
-            body: load_fixure('get_response_not_found'),
+            body: load_fixture('get_response_not_found'),
             headers: { 'Content-Type': 'application/json' }
         )
   end
 
   it 'gets a book from open library' do
-    uri = URI('https://openlibrary.org/api/books?bibkeys=ISBN:0385472579&format=json&jscmd=data')
-    response = Net::HTTP.get(uri)
-    expect(response).equal?(load_fixure('get_response_success'))
+    response = OpenLibrary.new.find_by_isbn('0385472579')
+    expect(response).equal?(load_fixture('get_response_success'))
   end
 
   it 'gets a book not found error from open library' do
-    uri = URI('https://openlibrary.org/api/books?bibkeys=ISBN:0385472579abc&format=json&jscmd=data')
-    response = Net::HTTP.get(uri)
-    expect(response).equal?(load_fixure('get_response_not_found'))
+    response = OpenLibrary.new.find_by_isbn('0385472579abc')
+  rescue OpenLibraryException
   end
 
-  def load_fixure(file_name)
+  def load_fixture(file_name)
     File.read(Rails.root.join('spec', 'support', 'fixtures', 'open_library', "#{file_name}.json"))
   end
 end
