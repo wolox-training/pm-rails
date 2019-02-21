67f15309e49f3bd12a7f5352c425befdda60167d
Paul Marclay <pmarclay@w3itsolutions.net>
2019-02-21 16:08:53 -0300

    added 2 tests, for success and not found, the stubs are in the same file

--- spec/services/open_library_spec.rb
+++ spec/services/open_library_spec.rb
@@ -1,8 +1,35 @@
 require 'rails_helper'
 
-describe OpenLibrary, type: :service do
-  it 'makes an external request' do
-    # expect(WebMock).to(have_requested(:get, '/api/v1/open_library/books/1234').with(body/query))
+describe OpenLibrary do
+  before do
+    stub_request(:get, 'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579&format=json&jscmd=data')
+        .to_return(
+            status: 200,
+            body: load_fixure('get_response_success'),
+            headers: { 'Content-Type': 'application/json' }
+        )
 
+    stub_request(:get, 'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579abc&format=json&jscmd=data')
+        .to_return(
+            status: 200,
+            body: load_fixure('get_response_not_found'),
+            headers: { 'Content-Type': 'application/json' }
+        )
+  end
+
+  it 'gets a book from open library' do
+    uri = URI('https://openlibrary.org/api/books?bibkeys=ISBN:0385472579&format=json&jscmd=data')
+    response = Net::HTTP.get(uri)
+    expect(response).equal?(load_fixure('get_response_success'))
+  end
+
+  it 'gets a book not found error from open library' do
+    uri = URI('https://openlibrary.org/api/books?bibkeys=ISBN:0385472579abc&format=json&jscmd=data')
+    response = Net::HTTP.get(uri)
+    expect(response).equal?(load_fixure('get_response_not_found'))
+  end
+
+  def load_fixure(file_name)
+    File.read(Rails.root.join('spec', 'support', 'fixtures', 'open_library', "#{file_name}.json"))
   end
 end
